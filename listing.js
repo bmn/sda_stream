c0077cb12f4aed8d400b783cdd131e8039ea41ce
Ian Bennett <webdev@ianbennett.net>
2010-04-21 11:35:59 +0100

    [0.3] Documentation, removal of index.html

--- listing.js
+++ listing.js
@@ -1,3 +1,14 @@
+/*
+  sda_stream
+  Copyright (c) Ian Bennett <webdev at ianbennett dot net> 2010.
+  
+  sda_stream is licensed under a Creative Commons Attribution-Share Alike 2.0
+    UK: England & Wales License.
+  <http://creativecommons.org/licenses/by-sa/2.0/uk/>
+  
+  The latest version of this software is available at:
+  <http://github.com/bmn/sda_stream/>
+*/
 function SDAStream(d) {
 
   // Defaults
